92b8851aa171529d93a9980ac1a108136a74463d
Aditya Yadav <103302549+adiyadav123@users.noreply.github.com>
2024-06-06 21:47:55 +0530

    Add analytics component to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
         >
           <ClerkProvider>{children}</ClerkProvider>
           <Toaster />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
